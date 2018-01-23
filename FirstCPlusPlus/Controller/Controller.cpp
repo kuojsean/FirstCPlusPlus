29f1de9ca8fc4449138770f7394ae617998aa6c4
kuojsean <kuo.j.sean@gmail.com>
2018-01-23 08:58:32 -0700

    Created the controller and runner files with a header for the controller.

---
+++ FirstCPlusPlus/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  FirstCPlusPlus
+//
+//  Created by Kuo, Sean on 1/23/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ FirstCPlusPlus/Controller/Runner.cpp
@@ -0,0 +1,16 @@
+//
+//  Runner.cpp
+//  FirstCPlusPlus
+//
+//  Created by Kuo, Sean on 1/23/18.
+//  Copyright © 2018 Kuo, Sean. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    
+    
+    return 0;
+}
